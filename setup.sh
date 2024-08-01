7c4b179d0fb81f31e8bedbc0af75aed4eb7f972d
tknr <geishagirlskoume@gmail.com>
2024-08-01 22:16:58 +0900

    u

---
+++ setup.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+curl -o luongz.jp.m3u https://raw.githubusercontent.com/luongz/iptv-jp/main/jp.m3u
+curl -o iptv-org.jp.m3u https://raw.githubusercontent.com/iptv-org/iptv/master/streams/jp.m3u
+curl -o iptv-org.jp_primehome.m3u https://raw.githubusercontent.com/iptv-org/iptv/master/streams/jp_primehome.m3u
