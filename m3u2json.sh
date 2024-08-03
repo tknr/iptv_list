0f7d6e4d8880b3e3f01b4bd35c29ef6be7e66a3e
tknr <geishagirlskoume@gmail.com>
2024-08-03 14:12:57 +0900

    u

---
+++ m3u2json.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+rm -f channels.csv
+touch channels.csv
+echo 'via,group-title,tvg_id,tvg_logo,name,url' >> channels.csv
+
+IFS=$'\n'
+
+for LINE in `cat m3u/luongz.jp.m3u | sed -z "s|\r\n|\n|g" |sed "s|png\", |png\" name=|g" | sed -z -e  "s|\nhttp| url=http|g" | grep "^#EXTINF" | grep -v "Information"`
+do
+	group_title=`echo ${LINE} | grep -oP 'group-title=\"([a-zA-z0-9\.\/\-]+)\"' | sed 's|group-title="\(.*\)"|\1|'`
+	tvg_id=`echo ${LINE} | grep -oP 'tvg-id=\"([a-zA-z0-9\.\/\-]+)\"' | sed 's|tvg-id="\(.*\)"|\1|'`	
+	tvg_logo=`echo ${LINE} | grep -oP 'tvg-logo=\"(.+)\"' | sed 's|tvg-logo="\(.*\)"|\1|'`
+	name=`echo ${LINE} | grep -oP 'name=(.+) url' | sed 's|name=\(.*\) url|\1|'`
+	url=`echo ${LINE} | grep -oP 'url=(.+)' | sed 's|url=\(.*\)|\1|'`
+	echo 'luongz.jp,'${group_title}','${tvg_id}','${tvg_logo}','${name}','${url} >> channels.csv
+done
+
--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-curl -o luongz.jp.m3u https://raw.githubusercontent.com/luongz/iptv-jp/main/jp.m3u
-curl -o iptv-org.jp.m3u https://raw.githubusercontent.com/iptv-org/iptv/master/streams/jp.m3u
-curl -o iptv-org.jp_primehome.m3u https://raw.githubusercontent.com/iptv-org/iptv/master/streams/jp_primehome.m3u
+mkdir -p m3u
+curl -o m3u/luongz.jp.m3u https://raw.githubusercontent.com/luongz/iptv-jp/main/jp.m3u
+curl -o m3u/iptv-org.jp.m3u https://raw.githubusercontent.com/iptv-org/iptv/master/streams/jp.m3u
+curl -o m3u/iptv-org.jp_primehome.m3u https://raw.githubusercontent.com/iptv-org/iptv/master/streams/jp_primehome.m3u
