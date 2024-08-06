ff5ab052ed68fca16c22bb78b2886bfa1cdc0caf
tknr <geishagirlskoume@gmail.com>
2024-08-06 14:26:35 +0900

    u

--- js/main.js
+++ js/main.js
@@ -6,9 +6,11 @@ $(function () {
 	}).done(function (data) {
 		const data_json = JSON.parse(JSON.stringify(data));
 		const modified_data = data_json.map(function (datum){
-			const img = (datum.tvg_logo) ? `<img src="${datum.tvg_logo}" width="64" />` : '';
-			const title = (datum.name) ? datum.name : datum.tvg_id;
-			datum.link = `<a target="_blank" href="${datum.url}">${img}${title}</a>`;
+			const img = (datum.tvg_logo) ? ` <img src="${datum.tvg_logo}" width="64" />` : '';
+			datum.title = (datum.name) ? datum.name : datum.tvg_id + '' + img;
+			datum.link = `<a target="_blank" href="${datum.url}"><i class="fa-solid fa-play"></i></a>`;
+			const vlc_scheme = `vlc-x-callback://x-callback-url/stream?url=`+encodeURI(datum.url);
+			datum.vlc = `<a href="${vlc_scheme}"><i class="fa-regular fa-circle-play"></i></a>`;
 			return datum;
 		});
 		console.log(modified_data);
@@ -37,7 +39,8 @@ $(function () {
 					{
 						data: 'via',
 						title: 'via',
-						orderable: true
+						orderable: true,
+						visible: true, 
 					},
 					{
 						data: 'group_title',
@@ -45,9 +48,28 @@ $(function () {
 						orderable: true
 					},
 					{
+						data: 'tvg_id',
+						title: 'tvg_id',
+						orderable: true,
+						visible: false, 
+					},
+					{
+						data: 'title',
+						title: 'title',
+						orderable: true,
+						visible: true, 
+					},
+					{
 						data: 'link',
 						title: 'link',
-						orderable: true
+						orderable: false,
+						visible: true, 
+					},
+					{
+						data: 'vlc',
+						title: 'vlc',
+						orderable: false,
+						visible: true, 
 					},
 				],
 				order: [
