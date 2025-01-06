53a0c3d44d7b3c03cf2a9cb16379269890f520fd
tknr <geishagirlskoume@gmail.com>
2025-01-06 17:15:28 +0900

    minify

---
+++ script/minify.js
@@ -0,0 +1,18 @@
+import fs from 'fs';
+import { minify } from 'minify';
+import tryToCatch from 'try-to-catch';
+
+const map = {
+    "src/js/main.js": "public/js/main.min.js",
+};
+
+for (const src in map) {
+    const dst = map[src];
+    console.log(src, dst);
+    const [error, data] = await tryToCatch(minify, src);
+    if (error) {
+        console.error(error);
+    } else {
+        fs.writeFile(dst, data);
+    }
+}
