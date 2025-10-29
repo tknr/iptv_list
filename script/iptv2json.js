688d0f3812a77a9d8d75a6a012f27431a3cc92b4
tk.hirai <tk.hirai@crusis.co.jp>
2025-10-29 11:30:03 +0900

    u

--- script/iptv2json.js
+++ script/iptv2json.js
@@ -13,85 +13,86 @@ let chArray = [];
 let urlArray = [];
 
 body_array.forEach((line) => {
-	// console.log(line);
-	if (line.startsWith('#EXTM3U')) {
-		return;
-	}
-	if (line.startsWith('#EXTINF')) {
-		console.log('line', line);
-		let line_array = line.split(',');
-		let chName = line_array[1];
+  console.log(line);
+  if (line.startsWith('#EXTM3U')) {
+    return;
+  }
+  if (line.startsWith('#EXTINF')) {
+    let line_array = line.split(',');
+    let chName = line_array[1];
 
-		let groupTitle = line_array[0].match(/group-title="([^"]+)"/)[1];
-		let tvgLogo = line_array[0].match(/tvg-logo="([^"]+)"/)[1];
+    let groupTitle = line_array[0].match(/group-title="([^"]+)"/)[1];
+    let tvgLogo = line_array[0].match(/tvg-logo="([^"]+)"/)[1];
 
-		let minifyLogo = minifyTvgLogo(tvgLogo).then((minifyLogo)=>{
-			console.log({'minifyLogo':minifyLogo});
-		        let datum = {
-                        	name: chName,
-                        	groupTitle: groupTitle,
-                        	tvgLogo: "image/" +getBaseFileName(tvgLogo),
-                	};
-                	console.log(datum);
-                	chArray.push(datum);
-		});
-		return;
-	}
-	if (line.startsWith('http')) {
-		console.log('line startswith http', line);
-		urlArray.push(line);
-		return;
-	}
+    minifyTvgLogo(tvgLogo);
+    let datum = {
+      name: chName,
+      groupTitle: groupTitle,
+      tvgLogo: "image/" + getBaseFileName(tvgLogo),
+    };
+    chArray.push(datum);
+    return;
+  }
+  if (line.startsWith('http')) {
+    console.log('line startswith http', line);
+    urlArray.push(line);
+    return;
+  }
 });
+
+console.log({ 'chArray': chArray });
+console.log({ 'urlArray': urlArray });
 
 chArray.forEach((datum, index) => {
-	let url = urlArray[index];
-	datum.url = url;
-	chArray[index] = datum;
+  let url = urlArray[index];
+  datum.url = url;
+  console.log({ 'datum': datum });
+  chArray[index] = datum;
 })
 
-console.log(chArray);
+console.log({ 'chArray': chArray });
 fs.writeFile('public/json/iptv-japan.json', JSON.stringify(chArray), err => {
-	if (err) {
-		console.error(err.message);
-		throw err;
-	}
+  if (err) {
+    console.error(err.message);
+    throw err;
+  }
 
-	console.log('data written to file');
+  console.log('data written to file');
 });
 
-function getBaseFileName(tvgLogo){
-	console.log(getBaseFileName.name, tvgLogo);
-	let baseFilename = tvgLogo.split('/').slice().reverse()[0];
-	return baseFilename;
+function getBaseFileName(tvgLogo) {
+  console.log(getBaseFileName.name, tvgLogo);
+  let baseFilename = tvgLogo.split('/').slice().reverse()[0];
+  return baseFilename;
 }
 
-async function minifyTvgLogo(tvgLogo){
-	console.log(minifyTvgLogo.name, tvgLogo);
+async function minifyTvgLogo(tvgLogo) {
+  console.log(minifyTvgLogo.name, tvgLogo);
 
-	let baseFileName = getBaseFileName(tvgLogo);
-	console.log({'baseFileName':baseFileName});
+  let baseFileName = getBaseFileName(tvgLogo);
+  console.log({ 'baseFileName': baseFileName });
 
-	const filename_sharpen = "public/image/" + baseFileName;
-	console.log({'filename_sharpen':filename_sharpen});
-	(async () => {
-		const imageBuffer = await got(tvgLogo).buffer();
+  const filename_sharpen = "public/image/" + baseFileName;
+  console.log({ 'filename_sharpen': filename_sharpen });
+  (async () => {
+    const imageBuffer = await got(tvgLogo).buffer();
 
-		// Resize the image using sharp
-		await sharp(imageBuffer)
-			.resize(64, null)
-			.png({
-				pallete: true,
-				effort: 10,
-				quality: 70,
-				compressionLevel: 9
-			})
-			.toFile(filename_sharpen, (err, info) => {
-				// console.error(err, info);
-				if (err) {
-					return tvgLogo;
-				}
-			});
-		return filename_sharpen;
-	})();
+    // Resize the image using sharp
+    sharp(imageBuffer)
+      .resize(64, null)
+      .png({
+        pallete: true,
+        effort: 10,
+        quality: 70,
+        compressionLevel: 9
+      })
+      .toFile(filename_sharpen, (err, info) => {
+        if (err) {
+          console.error(err);
+        }
+        if (info) {
+          // console.log(info);
+        }
+      });
+  })();
 }
