2f0b3a7f6e3a2d86d9d3f0a6f6ad6509b61673e0
tknr <geishagirlskoume@gmail.com>
2025-11-20 07:00:12 +0900

    2025年 11月 20日 木曜日 07:00:02 JST

---
+++ script/utako2json.js
@@ -0,0 +1,104 @@
+import got from 'got';
+import superagent from 'superagent';
+import { download as _download } from 'wget-improved';
+import fs from 'fs';
+import { createCommonJS } from 'mlly'
+const { __dirname, __filename, require } = createCommonJS(import.meta.url)
+import sharp from 'sharp'
+
+const response = await superagent.get("https://web.utako.moe/jp.m3u");
+console.log(response.body);
+const body_array = response.text.split(/\r\n|\r|\n/);
+
+let chArray = [];
+let urlArray = [];
+
+body_array.forEach((line) => {
+	// console.log(line);
+	if (line.startsWith('#EXTINF:-1 group-title="Information"')) {
+		return;
+	}
+	if (line.startsWith('https://google.com')) {
+		return;
+	}
+	if (line.startsWith('https://example.com')) {
+		return;
+	}
+	if (line.startsWith('https://mt01.utako.moe/test/intro.mp4/index.m3u8')) {
+		return;
+	}
+        if (line.startsWith('http://mt01.utako.moe:8001/radio')) {
+                return;
+        }
+	if (line.startsWith('#EXTINF')) {
+		console.log('line', line);
+		let line_array = line.split(',');
+		let chName = line_array[1];
+
+		let groupTitle = line_array[0].match(/group-title="([^"]+)"/)[1];
+		let tvgId = line_array[0].match(/tvg-id="([^"]+)"/)[1];
+		let tvgLogo = line_array[0].match(/tvg-logo="([^"]+)"/)[1];
+		console.log('chName', chName, 'groupTitle', groupTitle, 'tvgId', tvgId, 'tvgLogo', tvgLogo);
+
+		minifyTvgLogo(tvgLogo, tvgId);
+
+		let datum = {
+			name: chName,
+			groupTitle: groupTitle,
+			tvgId: tvgId,
+			tvgLogo: "image/" + tvgId + ".png",
+		};
+
+		chArray.push(datum);
+		return;
+	}
+	if (line.startsWith('http')) {
+		console.log('line startswith http', line);
+		urlArray.push(line);
+		return;
+	}
+});
+
+chArray.forEach((datum, index) => {
+	let url = urlArray[index];
+	datum.url = url;
+	chArray[index] = datum;
+})
+
+console.log(chArray);
+fs.writeFile('public/json/luongz.iptv-jp.json', JSON.stringify(chArray), err => {
+	if (err) {
+		console.log(err.message);
+
+		throw err;
+	}
+
+	console.log('data written to file');
+});
+
+
+function minifyTvgLogo(tvgLogo, tvgId) {
+	console.log(minifyTvgLogo.name, tvgLogo, tvgId);
+
+	const filename_sharpen = "public/image/" + tvgId + ".png";
+	(async () => {
+		const imageBuffer = await got(tvgLogo).buffer();
+
+		// Resize the image using sharp
+		await sharp(imageBuffer)
+			.resize(64, null)
+			.png({
+				pallete: true,
+				effort: 10,
+				quality: 70,
+				compressionLevel: 9
+			})
+			.toFile(filename_sharpen, (err, info) => {
+				// console.log(err, info);
+				if (err) {
+					return tvgLogo;
+				}
+			});
+		return filename_sharpen;
+	})();
+}
